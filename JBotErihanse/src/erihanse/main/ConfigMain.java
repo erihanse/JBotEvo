3c4d6968d3d2209f9b2d4a8acb765005b7ef58c5
Erik Aaron Hansen <erihanse@outlook.com>
2018-04-23 02:18:10 +0200

    Classpath resolve

--- JBotErihanse/src/erihanse/main/ConfigMain.java
+++ JBotErihanse/src/erihanse/main/ConfigMain.java
@@ -7,6 +7,7 @@ class ConfigMain {
         String classpath = System.getProperty("java.class.path");
         // new CombinedGui(new String[]{"--gui","classname=BlenderRenderer,enabledebugoptions=1,renderer=(classname=AdHocNetworkRenderer))"});
         // new CombinedGui(new String[]{"--gui","classname=ResultViewerGui,renderer=(classname=TraceRenderer))"});
-        new CombinedGui(new String[]{"--gui","classname=ResultViewerGui,renderer=(classname=AdHocNetworkRenderer))"});
+        new CombinedGui(
+                new String[] { "--gui", "classname=ResultViewerGui,renderer=(classname=AdHocNetworkRenderer))" });
     }
 }
