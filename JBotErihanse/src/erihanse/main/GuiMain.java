bcff0dabf5df4effd4e66fbc0b62d1bafd06aefc
Erik Aaron Hansen <erihanse@outlook.com>
2018-04-23 04:43:23 +0200

    Update

--- JBotErihanse/src/erihanse/main/GuiMain.java
+++ JBotErihanse/src/erihanse/main/GuiMain.java
@@ -4,7 +4,6 @@ import gui.CombinedGui;
 
 class GuiMain {
     public static void main(String[] args) {
-        String classpath = System.getProperty("java.class.path");
         // new CombinedGui(new String[]{"--gui","classname=BlenderRenderer,enabledebugoptions=1,renderer=(classname=AdHocNetworkRenderer))"});
         // new CombinedGui(new String[]{"--gui","classname=ResultViewerGui,renderer=(classname=TraceRenderer))"});
         new CombinedGui(
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ To launch a GUI for specifying which controller, robot, environment, etc. to use
 launch `GuiMain`.
 
 ## Automator
-Experiments can be used by using the [Evolution Automator](https://github.com/BioMachinesLab/jbotevolver/wiki/Evolution-Automator).
+Experiments can be run by using the [Evolution Automator](https://github.com/BioMachinesLab/jbotevolver/wiki/Evolution-Automator).
 To do this, launch `Automator`.
